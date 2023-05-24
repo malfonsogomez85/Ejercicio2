d0a0156250c134185e9b5ea62689a08aecfb3fbf
monicaalfonso <mariamonicaalfonso5@gmail.com>
2023-05-23 23:58:31 -0500

    Integrantes trabajo 2

---
+++ TRABAJO2.py
@@ -0,0 +1,13 @@
+#INTEGRANTES
+#-DARWIN JOAN ESQUIVEL ARENAS
+#-JHON EDWARD SANCHEZ CASTILLO
+#-Jhon Freddy Bernal Sarmiento
+#-JUAN MANUEL MARTINEZ MARTINEZ
+#-MARÍA MÓNICA ALFONSO GÓMEZ
+ 
+import numpy as np
+
+nomdiv={'COP':'Peso Colombiano', 'GBP':'Libra Esterlina', 'USD':'Dolar Americano', 'JPY':'Yen','EUR':'Euro'}
+# Se emplea un diccionario para indexar los nombres de las divisas y emplear una referencia rapida para llamarla
+
+trm=np.arange(5,3, dtype=float)
